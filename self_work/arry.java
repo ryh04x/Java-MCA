72e263ae5f495ae65666ffe6114adbe1ccb63219
ryh04x <work4rhythm@gmail.com>
2024-09-05 16:51:50 +0530

    declaring 1d, 2d and string array

---
+++ self_work/arry.java
@@ -0,0 +1,36 @@
+package self_work;
+
+public class arry {
+
+    // Array of in (1 d arry)
+
+    public static void main(String[] args) {
+
+        int[] marks = { 1, 2, 3, 4, 5 };
+
+        // System.out.println(marks[4]);
+
+        for (int value : marks) {
+
+            System.out.println(value);
+
+        }
+
+            String cars[] = { "audi", "bmw", "jaguar", "mg", "maruti" };
+
+            for (String meow : cars) {
+
+                System.out.println(meow);
+
+            }
+
+
+            int [][] matrix = {{1, 2, 3} , {4, 5, 6}};
+            System.out.println(matrix[1][0]);
+
+
+        }
+
+    }
+
+
