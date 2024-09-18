e29ee78531470b002db427047ebc6606bbe56ab7
ryh04x <work4rhythm@gmail.com>
2024-09-18 16:12:41 +0530

    method array elements changing

--- self_work/CWH_thirtytwo.java
+++ self_work/CWH_thirtytwo.java
@@ -2,6 +2,20 @@ public class CWH_thirtytwo {
     
     public static class cwh_methodoverloading{
 
+        // Arry changing
+        
+        static void change(int a){
+            a = 98;
+        }
+    
+        static void change2(int [] arr){
+            arr[0] = 98;
+        }
+
+
+        // Method overloading 
+
+
         static void foo(){
             System.out.println("Good Morning!");
         }
@@ -18,6 +32,20 @@ public class CWH_thirtytwo {
     }
 
     public static void main(String[] args) {
+
+        // Case 1: Changing the Integer
+
+        int x = 45;
+        cwh_methodoverloading.change(x);
+        System.out.println("The value of x after running change is: " + x);
+
+        // Case 1: Changing the Array
+
+        int [] marks = {52, 73, 77, 89, 98, 94};
+        cwh_methodoverloading.change2(marks);
+        System.out.println("The value of x after running change is: " + marks[0]);
+
+
         cwh_methodoverloading.foo();
         cwh_methodoverloading.foo(3000);
         cwh_methodoverloading.foo(3000, 4000);
--- self_work/arry.java
+++ self_work/arry.java
@@ -1,4 +1,3 @@
-package self_work;
 
 public class arry {
 
